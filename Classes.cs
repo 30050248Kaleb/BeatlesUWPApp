b898baad95b2d2c7f0d062d2042bf63482e51d5d
30050248Kaleb <87620908+30050248Kaleb@users.noreply.github.com>
2023-03-20 15:35:31 +1300

    Add project files.

---
+++ Classes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+
+namespace BeatlesApp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string ProfileImage { get; set; }
+        public string FirstLastName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public Person(string fname, string lname, string fullname, string profImage)
+        {
+            FirstName = fname;
+            LastName = lname;
+            FullName = fullname;
+            ProfileImage = profImage;
+        }
+    }
+
+    public class Musician : Person
+    {
+
+
+
+        public Musician(string fname, string lname, string fullname, string profImage) : base(fname, lname, fullname, profImage)
+        {
+            
+        }
+    }
+
+    public class Band
+    {
+        public string Name { get; set; }
+        public List<Musician> Members { get; set; }
+
+        public Band()
+        {
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using BeatlesApp.Pages;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BeatlesApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 400));
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            MainFrame.Navigate(typeof(Homepage));
+            Navbar.SelectedItem = Navbar.MenuItems[1];
+        }
+
+        private void Page1NavItemTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (MainFrame.SourcePageType != typeof(Homepage))
+            {
+                MainFrame.Navigate(typeof(Homepage));
+            }
+        }
+
+        private void Page2NavItemTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (MainFrame.SourcePageType != typeof(Homepage))
+            {
+                MainFrame.Navigate(typeof(Homepage));
+            }
+        }
+
+        private void ChangeSizePhoneTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().TryResizeView(new Size(368, 636));
+        }
+
+        private void ChangeSizeDesktopTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().TryResizeView(new Size(1288, 715));
+        }
+
+        private void ChangeSizeXboxTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().TryResizeView(new Size(976, 538));
+        }
+    }
+}
